90fb31dbdbc19804ba8abd77f1b03c00c33fbe32
Jules BOURDAIS <jules.bourdais@kitware.com>
2022-06-03 13:19:54 +0200

    initial commit: vr

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+module.exports = {
+    resolve: {
+        symlinks: false
+    },
+    devtool: 'source-map',
+    devServer: {
+      hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.glsl$/i,
+                loader: 'shader-loader',
+              },
+              {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'Sources'),
+                use: [
+                  {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env'],
+                    },
+                  },
+                ],
+              },
+              {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: [
+                  { loader: 'style-loader' },
+                  { loader: 'css-loader' },
+                  { loader: 'postcss-loader' },
+                ],
+              },
+              {
+                test: /\.html$/,
+                use: [
+                  { loader: 'html-loader' },
+                ],
+              },
+              {
+                test: /\.module\.css$/,
+                use: [
+                  { loader: 'style-loader' },
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: {
+                        localIdentName: '[name]-[local]_[sha512:hash:base64:5]',
+                      },
+                    },
+                  },
+                  { loader: 'postcss-loader' },
+                ],
+              },
+              {
+                test: /\.svg$/,
+                type: 'asset/source',
+              },
+              {
+                test: /\.worker\.js$/,
+                use: [{ loader: 'worker-loader', options: { inline: 'no-fallback' } }],
+              },
+        ]
+    }
+}
